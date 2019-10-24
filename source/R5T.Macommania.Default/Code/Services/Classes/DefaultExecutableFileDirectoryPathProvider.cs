7f9ce1d7f817f27758291575e45eecbdd8410936
MinexAutomation <minex.automation@gmail.com>
2019-10-23 21:53:49 -0700

    Implemented service interface providing the directory path of the currently executing executable file. This kinda a trivial service, but ok for strict separation of concerns purposes.

---
+++ source/R5T.Macommania.Default/Code/Services/Classes/DefaultExecutableFileDirectoryPathProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using R5T.Lombardy;
+
+
+namespace R5T.Macommania.Default
+{
+    public class DefaultExecutableFileDirectoryPathProvider : IExecutableFileDirectoryPathProvider
+    {
+        private IExecutableFilePathProvider ExecutableFilePathProvider { get; }
+        private IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public DefaultExecutableFileDirectoryPathProvider(IExecutableFilePathProvider executableFilePathProvider, IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.ExecutableFilePathProvider = executableFilePathProvider;
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        public string GetExecutableFileDirectoryPath()
+        {
+            var executableFilePath = this.ExecutableFilePathProvider.GetExecutableFilePath();
+
+            var executableFileDirectoryPath = this.StringlyTypedPathOperator.GetDirectoryPathForFilePath(executableFilePath);
+            return executableFileDirectoryPath;
+        }
+    }
+}
