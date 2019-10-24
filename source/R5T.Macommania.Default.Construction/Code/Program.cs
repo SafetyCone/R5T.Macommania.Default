9ebf4138cbd7faac8758ff3ad54241596bf24288
MinexAutomation <minex.automation@gmail.com>
2019-10-23 21:03:31 -0700

    Initial commit of source-stub.

---
+++ source/R5T.Macommania.Default.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Macommania.Default.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Macommania.Default/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Macommania.Default
+{
+    public class Class1
+    {
+    }
+}
