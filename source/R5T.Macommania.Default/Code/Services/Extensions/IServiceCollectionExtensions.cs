cac26646e8a4c83e2c7f0fd2f5d33dfc7c0ff3db
MinexAutomation <minex.automation@gmail.com>
2019-12-08 16:54:32 -0800

    Added AddX() method for default executable file directory path provider.

---
+++ source/R5T.Macommania.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia.Extensions;
+using R5T.Lombardy;
+
+
+namespace R5T.Macommania.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDefaultExecutableFileDirectoryPathProvider<TStringlyTypedPathOperator>(this IServiceCollection services)
+            where TStringlyTypedPathOperator: class, IStringlyTypedPathOperator
+        {
+            services
+                .TryAddSingletonFluent<IStringlyTypedPathOperator, TStringlyTypedPathOperator>()
+                .TryAddSingletonFluent<IExecutableFilePathProvider, DefaultExecutableFilePathProvider>()
+                .AddSingleton<IExecutableFileDirectoryPathProvider, DefaultExecutableFileDirectoryPathProvider>()
+                ;
+
+            return services;
+        }
+    }
+}
