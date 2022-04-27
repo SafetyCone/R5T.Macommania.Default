b215d2a8d1a9d842d576ebd14a6dd6d29ce69ca0
davidcoats <david@getsafetycone.com>
2022-04-27 15:32:39 -0700

    Interim changes.

---
+++ source/R5T.Macommania.Default/Code/Services/Implementations/ExecutableFileDirectoryPathProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using R5T.Lombardy;
+
+using R5T.T0064;
+
+
+namespace R5T.Macommania.Default
+{
+    [ServiceImplementationMarker]
+    public class ExecutableFileDirectoryPathProvider : IExecutableFileDirectoryPathProvider, IServiceImplementation
+    {
+        private IExecutableFilePathProvider ExecutableFilePathProvider { get; }
+        private IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public ExecutableFileDirectoryPathProvider(IExecutableFilePathProvider executableFilePathProvider, IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.ExecutableFilePathProvider = executableFilePathProvider;
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        public string GetExecutableFileDirectoryPath()
+        {
+            var executableFilePath = this.ExecutableFilePathProvider.GetExecutableFilePath();
+
+            var executableFileDirectoryPath = this.StringlyTypedPathOperator.GetDirectoryPathForFilePath(executableFilePath);
+            return executableFileDirectoryPath;
+        }
+    }
+}
---
+++ source/R5T.Macommania.Default/Code/Services/Implementations/ExecutableFilePathProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Macommania.Default
+{
+    [ServiceImplementationMarker]
+    public class ExecutableFilePathProvider : IExecutableFilePathProvider, IServiceImplementation
+    {
+        public string GetExecutableFilePath()
+        {
+            var output = Utilities.GetExecutableFilePathValue();
+            return output;
+        }
+    }
+}
